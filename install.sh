8f81c208c6e2c3ce0150fce38c872bb5ed01ee78
kDot <k-Laus@gmx.net>
2016-09-03 14:12:48 +0200

    Update install.sh
    enable automatic installation

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo Does not yet work automatically. Please execute followign commands manually
-echo git submodule init 
-echo git submodule update
-echo cp -r filesToMove/.vim ~/
-echo cp -r filesToMove/.vimrc ~/
-echo cp -r filesToMove/.zsh ~/
-echo cp -r filesToMove/.zshrc ~/
+
+git submodule init 
+git submodule update
+cp -r filesToMove/.vim ~/
+cp -r filesToMove/.vimrc ~/
+cp -r filesToMove/.zsh ~/
+cp -r filesToMove/.zshrc ~/
+echo "Done. Have fun.."
